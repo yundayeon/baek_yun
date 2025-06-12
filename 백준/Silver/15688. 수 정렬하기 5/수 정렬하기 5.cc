86a04a6c7404166341f457a412aab26dfa8b1e47
day_31 <yunda0331@gmail.com>
2025-06-12 11:30:00 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4200 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i =0; i < n; i++) {
+        cin>>arr[i];
+    }
+
+    sort(arr,arr+n);
+    for (int i = 0; i <n; i++) {
+        cout<<arr[i]<<'\n';
+    }
+}
