7d4b4ef8155c6c8542c0889702eaf9b78fa9f2cc
day_31 <yunda0331@gmail.com>
2025-06-12 12:19:05 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    string s;
+    cin>>s;
+
+    sort(s.begin(),s.end(),greater<int>());
+    cout<<s;
+}
