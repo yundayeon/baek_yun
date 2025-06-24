efeb01349c4be93b19333e36eebab0397f8596d0
day_31 <yunda0331@gmail.com>
2025-06-24 16:14:45 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cout<<"|\\_/|"<<'\n';
+    cout<<"|q p|   /}"<<'\n';
+    cout<<"( 0 )\"\"\"\\"<<'\n';
+    cout<<"|\"^\"`    |"<<'\n';
+    cout<<"||_/=\\\\__|";
+
+}
