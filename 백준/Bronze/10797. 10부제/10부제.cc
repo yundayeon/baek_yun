a18039b3b36e3540312e3a35acc1f564abd1832a
day_31 <yunda0331@gmail.com>
2025-07-13 03:48:04 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int a;
+    cin>>a;
+    int arr[5];
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        cin>>arr[i];
+        if (arr[i] == a) {
+            sum++;
+        }
+    }cout<<sum;
+}
