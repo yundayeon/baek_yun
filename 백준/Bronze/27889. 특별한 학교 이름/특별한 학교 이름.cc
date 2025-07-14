0536c89ad39f6574a7409b31cfaaecc0b7114501
day_31 <yunda0331@gmail.com>
2025-07-14 09:59:49 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    string s;
+    cin>>s;
+    if (s == "NLCS") {
+        cout<<"North London Collegiate School";
+    } else if (s == "BHA") {
+        cout<<"Branksome Hall Asia";
+    } else if (s == "KIS") {
+        cout<<"Korea International School";
+    } else if (s == "SJA") {
+        cout<<"St. Johnsbury Academy";
+    }
+}
