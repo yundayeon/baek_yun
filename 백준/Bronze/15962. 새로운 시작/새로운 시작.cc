c0712b46c03df69a255609e636b5227201375b82
day_31 <yunda0331@gmail.com>
2025-07-25 22:10:45 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,10 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cout<<"파이팅!!";
+}
