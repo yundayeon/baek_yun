f43fe29e41df3d685e14e7635cf7c8486593e522
day_31 <yunda0331@gmail.com>
2025-08-04 21:46:47 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,11 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+        cout<<"강한친구 대한육군"<<'\n';
+        cout<<"강한친구 대한육군";
+}
