7dd9420d3c985d7ce13c4e821f758b7a92f00582
day_31 <yunda0331@gmail.com>
2026-04-06 15:42:55 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    priority_queue<int,vector<int>,greater<int>>p;
+    int n;
+    cin>>n;
+    int a;
+    for (int i = 0; i < n; i++) {
+        cin>>a;
+        if (a == 0 && p.empty() == 1) {
+            cout<<'0'<<'\n';
+        }else if (a == 0) {
+            cout<<p.top()<<'\n';
+            p.pop();
+        }else p.push(a);
+    }
+}
